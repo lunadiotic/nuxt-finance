a6faf2ebf068a2b1adb2022416626dfee994ee8b
aim <ahmadhakim404@gmail.com>
2024-03-20 14:16:23 +0700

    membuat layout utama finance tracker

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<script setup>
+useHead({
+	link: [
+		{
+			rel: 'preconnect',
+			href: 'https://fonts.googleapis.com',
+		},
+		{
+			rel: 'preconnect',
+			href: 'https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&display=swap',
+		},
+	],
+});
+</script>
+
+<template>
+	<div class="container mx-auto max-w-4xl">
+		<main class="my-10">
+			<slot />
+		</main>
+	</div>
+</template>
+
+<style>
+body {
+	font-family: 'Inter', sans-serif;
+}
+
+body {
+	@apply bg-white dark:bg-gray-900;
+}
+</style>
