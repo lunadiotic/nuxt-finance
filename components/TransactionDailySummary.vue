7617335f659ae6b9318a0ad8843d3f67ae514f3e
aim <ahmadhakim404@gmail.com>
2024-04-08 22:32:57 +0700

    mengelompokkan data transaksi perhari

---
+++ components/TransactionDailySummary.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+	date: String,
+	transactions: Array,
+});
+
+const sum = computed(() => {
+	let sum = 0;
+	for (const transaction of props.transactions) {
+		if (transaction.type === 'income') {
+			sum += transaction.amount;
+		} else {
+			sum -= transaction.amount;
+		}
+	}
+	return sum;
+});
+
+const { currency: amount } = useCurrency(sum);
+</script>
+
+<template>
+	<div
+		class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-500 dark:text-gray-400"
+	>
+		<div class="flex items-center justify-between">
+			{{ date }}
+		</div>
+		<div class="flex items-center justify-end">
+			{{ amount }}
+		</div>
+	</div>
+</template>
