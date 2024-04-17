6fc03e77b6bd222e09a97cef7d0405fbcd690ee1
aim <ahmadhakim404@gmail.com>
2024-04-17 22:56:31 +0700

    desain state form transaction

--- components/TransactionModal.vue
+++ components/TransactionModal.vue
@@ -12,6 +12,13 @@ const isModalOpen = computed({
 		emit('update:modelValue', value);
 	},
 });
+
+const state = ref({
+	date: new Date().toISOString().split('T')[0],
+	description: undefined,
+	type: transactionTypeOptions[0],
+	amount: 0,
+});
 </script>
 
 <template>
@@ -34,36 +41,49 @@ const isModalOpen = computed({
 				</div>
 			</template>
 
-			<UFormGroup label="Date" :required="true" name="created_at" class="mb-4">
-				<UInput
-					type="date"
-					placeholder="Date"
-					icon="i-heroicons-calendar-20-solid"
-				/>
-			</UFormGroup>
+			<UForm :state="state">
+				<UFormGroup
+					label="Date"
+					:required="true"
+					name="created_at"
+					class="mb-4"
+				>
+					<UInput
+						type="date"
+						placeholder="Date"
+						icon="i-heroicons-calendar-20-solid"
+						v-model="state.date"
+					/>
+				</UFormGroup>
 
-			<UFormGroup
-				label="Description"
-				:required="true"
-				name="description"
-				class="mb-4"
-			>
-				<UInput type="text" placeholder="Description" />
-			</UFormGroup>
+				<UFormGroup
+					label="Description"
+					:required="true"
+					name="description"
+					class="mb-4"
+				>
+					<UInput
+						type="text"
+						placeholder="Description"
+						v-model="state.description"
+					/>
+				</UFormGroup>
 
-			<UFormGroup label="Type" :required="true" name="type" class="mb-4">
-				<USelect
-					type="text"
-					placeholder="type of transaction"
-					:options="transactionTypeOptions"
-				/>
-			</UFormGroup>
+				<UFormGroup label="Type" :required="true" name="type" class="mb-4">
+					<USelect
+						type="text"
+						placeholder="type of transaction"
+						:options="transactionTypeOptions"
+						v-model="state.type"
+					/>
+				</UFormGroup>
 
-			<UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-				<UInput type="number" placeholder="Amount" />
-			</UFormGroup>
+				<UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+					<UInput type="number" placeholder="Amount" v-model="state.amount" />
+				</UFormGroup>
 
-			<UButton type="submit" color="black" variant="solid">Submit</UButton>
+				<UButton type="submit" color="black" variant="solid">Submit</UButton>
+			</UForm>
 		</UCard>
 	</UModal>
 </template>
